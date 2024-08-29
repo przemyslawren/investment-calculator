c70e6ddaed467d3302c457c96715e05505fea591
przemyslawren <przemyslaw.ren@gmail.com>
2024-08-29 17:39:29 +0200

    finish app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Result from "./components/Result.jsx";
+import { useState } from "react";
+import InfoMessage from "./components/InfoMessage.jsx";
+
+const INITIAL_INVESTMENT = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
+
 function App() {
+  const [userInput, setUserInput] = useState(INITIAL_INVESTMENT);
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <main>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {userInput.duration < 1 ? (
+        <InfoMessage />
+      ) : (
+        <Result userInput={userInput} />
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/InfoMessage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InfoMessage = () => {
+  return <p className="center">Duration must equal at least 1!</p>;
+};
+
+export default InfoMessage;
